ede79b8b2701de13c1d375853687e7fe71a917ca
Saint Thomas Academy EVT <staevt@hutman.net>
2012-05-20 15:58:42 -0500

    Fixed silly sensors bugs
    - Added sensors.hex to Makefile 'all' target
    - Fixed end-of-message indicator bug in sensors.cpp

--- arduino/sensors.cpp
+++ arduino/sensors.cpp
@@ -1,14 +1,43 @@
 #include <Arduino.h>
 
+
+// Converts the ASCII character c from base 2-36 to an integer
+int ctoi(int c)
+{
+	if('0' <= c && c <= '9')
+		return c - '0';
+	
+	c = tolower(c);
+	if('a' <= c && c <= 'z')
+		return c - 'a';
+	
+	// Invlaid value
+	return -1;
+}
+
 void setup()
 {
 	Serial.begin(115200);
 }
 
-void loop()
-{
-}
+void loop() {}
 
 void serialEvent()
 {
+	int cmd = Serial.read(), pin;
+	
+	if(cmd == 'a' || cmd == 'd')
+	{
+		do {
+			// Wait for a character
+			while(!Serial.available());
+			
+			pin = ctoi(Serial.read());
+			
+			// Respond
+			Serial.print(cmd == 'a' ? analogRead(pin) : digitalRead(pin));
+		} while(pin >= 0);
+		
+		Serial.print('\0');
+	}
 }
