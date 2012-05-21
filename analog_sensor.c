a3b9f001199fea88bc64bde35d226eeed4174d97
Saint Thomas Academy EVT <staevt@hutman.net>
2012-05-20 20:58:26 -0500

    Added cool analog sensor bar; set frame delay to 0

--- analog_sensor.c
+++ analog_sensor.c
@@ -1,6 +1,9 @@
 #include "angel.h"
 
 
+#define MAX_VAL 1023
+
+
 static void analog_draw(draw_task_t *task);
 
 draw_task_funcs_t analog_draw_funcs = {
@@ -9,7 +12,17 @@ draw_task_funcs_t analog_draw_funcs = {
 
 static void analog_draw(draw_task_t *task)
 {
+	int val;
 	char valstr[20];
+	
 	serial_cmd(valstr,20,"a0");
 	print_text(valstr,task->area.x,task->area.y,50,50,0,0,0);
+	
+	val = atoi(valstr);
+	rectangleRGBA(screen,task->area.x,task->area.y + task->area.h/2,
+		task->area.x + task->area.w - 1,task->area.y + task->area.h/2 + 50,
+		50,50,255,255);
+	boxRGBA(screen,task->area.x + 1,task->area.y + task->area.h/2 + 1,
+		task->area.x + (task->area.w - 2)*val/MAX_VAL,
+		task->area.y + task->area.h/2 + 49,255,0,0,255);
 }
--- angel.h
+++ angel.h
@@ -11,7 +11,7 @@
 
 #define MAIN_FONT "FreeSans.ttf"
 
-#define UPDATE_DELAY_MS 1000
+#define UPDATE_DELAY_MS 0
 
 
 typedef struct draw_task draw_task_t;
