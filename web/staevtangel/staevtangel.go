ff3b8cc3877a0ab543c844f015cd5609549c3cf2
Saint Thomas Academy EVT <staevt@hutman.net>
2012-05-22 16:34:26 -0500

    Some real coding for the backend

--- web/staevtangel/staevtangel.go
+++ web/staevtangel/staevtangel.go
@@ -1,14 +1,35 @@
 package staevtangel
 
-import {
+import (
+	"appengine"
+	"appengine/user"
 	"fmt"
 	"net/http"
-}
+)
 
 func init() {
-	http.HandleFunc("/",rootHandler)
+	http.HandleFunc("/",monitorHandler)
 }
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w,"Successful test of rootHandler")
+func monitorHandler(w http.ResponseWriter, r *http.Request) {
+	checkUser(w,r)
+	
+	fmt.Fprint(w,"Cool info goes here.")
+}
+
+// Log-in is required for basically the entire site
+func checkUser(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	
+	u := user.Current(c)
+	if u == nil {
+		url, err := user.LoginURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusFound)
+		return
+	}
 }
