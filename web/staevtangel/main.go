7b72e29388222ec9d2842077bef2b52dd87e9743
Saint Thomas Academy EVT <staevt@hutman.net>
2012-05-24 22:38:48 -0500

    Restricted access to cadets.com logins

---
+++ web/staevtangel/main.go
@@ -0,0 +1,33 @@
+package staevtangel
+
+import (
+	"appengine"
+	"appengine/user"
+	
+	"fmt"
+	"net/http"
+	"regexp"
+)
+
+func init() {
+	http.HandleFunc("/",monitorHandler)
+}
+
+func monitorHandler(w http.ResponseWriter, r *http.Request) {
+	if !userAuthorized(w,r) {
+		http.Error(w,"User not authorized",http.StatusTeapot)
+		return
+	}
+	
+	logoutURL, _ := user.LogoutURL(appengine.NewContext(r),r.URL.String())
+	fmt.Fprintf(w,"<html><body>Cool info goes here.<br>Hi, %s!<br><a href=\"%v\">Logout</a></body></html>",user.Current(appengine.NewContext(r)).Email,logoutURL)
+}
+
+func userAuthorized(w http.ResponseWriter, r *http.Request) bool {
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	
+	matched, _ := regexp.MatchString("@cadets.com$",u.Email)
+	
+	return matched
+}
