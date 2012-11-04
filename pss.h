efc37027d6692baebd137db6735a9cb93accf013
Saint Thomas Academy EVT <staevt@hutman.net>
2012-11-03 21:32:22 -0500

    Shifting to rev. 2 of the UI description language

--- pss.h
+++
@@ -1,9 +0,0 @@
-typedef struct pss_attr {
-	struct pss_attr *prev;
-	struct pss_attr *next;
-} pss_attr_t;
-
-typedef struct pss_block {
-	struct pss_block *prev;
-	struct pss_block *next;
-} pss_block_t;
